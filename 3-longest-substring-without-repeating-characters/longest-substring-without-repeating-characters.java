c02e93f6a8a3c0d98dcc4be37cce0b21ce64753a
KALI <96970686+kalimuthuC4@users.noreply.github.com>
2024-08-08 19:16:50 +0530

    Time: 443 ms (5.02%) | Memory: 45.3 MB (8.88%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -8,17 +8,14 @@ class Solution {
         int m = 0,l=0,r=0;
         while(r<s.length())
         {
-            if(!h.contains(s.charAt(r))){
-                h.add(s.charAt(r));
-                r++;
-                m = Math.max(m,r-l);
-                            // System.out.print(" "+ l+"->"+r);
-
-            }
-            else{
-                h.remove(s.charAt(l));
-                l++;
-            }
+          char c = s.charAt(r);
+          while(h.contains(c)){
+            h.remove(s.charAt(l));
+            l++;
+          }
+          h.add(c);
+          r++;
+          m = Math.max(m,r-l);
             
             System.out.print(" "+ m);
             System.out.print(h);
