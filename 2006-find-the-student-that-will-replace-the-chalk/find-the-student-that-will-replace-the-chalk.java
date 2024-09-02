a4844f43d1227f8ba8e9c8ff245efa186daaf30e
KALI <96970686+kalimuthuC4@users.noreply.github.com>
2024-09-02 11:34:21 +0530

    Time: 3 ms (30.80%) | Memory: 59.5 MB (87.17%) - LeetSync

--- 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.java
+++ 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.java
@@ -1,15 +1,19 @@
 class Solution {
     public int chalkReplacer(int[] chalk, int k) {
-        int i;
-        while(k >= 0){
-            for(i = 0; i < chalk.length; i++){
-                if(chalk[i] <= k)
-                    k -= chalk[i];
-                else
-                    return i;
-            }
-            // System.out.println(k);
+        long s = 0;
+        for(int i = 0; i < chalk.length; i++) s += chalk[i];
+        int rem = (int)(k % s);
+        if(rem == 0)
+            return 0;
+        System.out.print(rem);
+        for(int i = 0; i < chalk.length; i++){
+            if(rem >= chalk[i])
+                rem -= chalk[i];
+            else
+                return i;
         }
+        if(rem > 0)
+            return 1;
         return 0;
     }
 }
